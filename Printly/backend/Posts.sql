5e425d97116ec4ea96e3584b1c12c7a74b158623
PBC5 <pau2000vlc@gmail.com>
2025-05-27 20:56:23 +0200

    sdfsf

---
+++ Printly/backend/Posts.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario TEXT,
+    titulo TEXT,
+    ext TEXT
+);
+INSERT INTO posts (usuario, titulo, ext) VALUES ('asd', 'asd', 'obj');
+
+INSERT INTO posts (usuario, titulo, ext) VALUES ('asd', 'asd', 'obj');
+
+INSERT INTO posts (usuario, titulo, ext) VALUES ('asd', 'asd', 'obj');
+
+INSERT INTO posts (usuario, titulo, ext) VALUES ('asd', 'asd', 'obj');
